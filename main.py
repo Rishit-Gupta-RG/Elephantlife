7e0edf749ac9ea4da7c33dfce7b9ec76ce0a2847
Rishit Gupta <gamingsandroking@gmail.com>
2024-04-12 01:46:09 +0530

    made it work

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import disnake
 from disnake.ext import commands
 from disnake.ext import tasks
 from disnake import TextInputStyle
-import re
 import asyncio
+from typing import List
+from fuzzywuzzy import process
+from PIL import Image
 import os
 import io
 import aiohttp
@@ -41,31 +43,55 @@ class GifType(disnake.ui.Modal):
 
 image_extensions_pattern = r'\.(jpg|jpeg|png|gif|)$'
 
-@bot.message_command(name="Gif generator")
-async def gif_gen(inter: disnake.MessageCommand, message: disnake.Message):
-    if message.attachments:
-        attachment = message.attachments[0]
-        if attachment.content_type.startswith('image'):
-            url = attachment.url
-            modal = GifType(url)
-            await inter.response.send_modal(modal=modal)
+@bot.user_command(name="Create Gif")
+async def gif_gen(inter: disnake.UserCommandInteraction, user: disnake.User):
+    await inter.response.send_modal(modal=GifType(user.display_avatar.url))
+
+gif_types = ['abstract', 'ads', 'balls', 'bayer', 'bevel', 'billboard', 'blocks', 'blur', 'boil', 'bomb', 'bonks', 'bubble', 'burn', 'canny', 'cartoon', 'cinema', 'clock', 'cloth', 'contour', 'cow', 'cracks', 'cube', 'dilate', 'dither', 'dots', 'earthquake', 'emojify', 'endless', 'equations', 'explicit', 'fall', 'fan', 'fire', 'flag', 'flush', 'gallery', 'gameboy_camera', 'glitch', 'globe', 'half_invert', 'heart_diffraction', 'hearts', 'infinity', 'ipcam', 'kanye', 'knit', 'lamp', 'laundry', 'layers', 'letters', 'lines', 'liquefy', 'logoff', 'lsd', 'magnify', 'matrix', 'melt', 'minecraft', 'neon', 'optics', 'painting', 'paparazzi', 'patpat', 'pattern', 'phase', 'phone', 'pizza', 'plank', 'plates', 'poly', 'print', 'pyramid', 'radiate', 'rain', 'reflection', 'ripped', 'ripple', 'roll', 'sensitive', 'shear', 'shine', 'shock', 'shoot', 'shred', 'slice', 'soap', 'spikes', 'spin', 'stereo', 'stretch', 'tiles', 'tunnel', 'tv', 'wall', 'warp', 'wave', 'wiggle', 'zonk']
+async def autocomplete_gif_types(inter, string: str) -> List[str]:
+    string = string.lower()
+    match, _ = process.extractOne(string, gif_types)
+    return [match]
+
+@bot.slash_command(name="gif", description="Creates a nice GIF")
+async def gif(inter: disnake.ApplicationCommandInteraction, image: disnake.Attachment, type: str = commands.Param(autocomplete=autocomplete_gif_types)):
+        """
+        Parameters
+        ----------
+        image: Upload the image you need to make a GIF of
+        type: Select the GIF type
+        """
+        if type.lower() not in gif_types:
+            await inter.response.send_message("Wrong GIF type.\nPlease select the GIF type from <https://discord.com/channels/1163511761446637681/1198637019660816394/1223339737813614612>, and make sure to type it correctly.", ephemeral=True)
         else:
-            await inter.response.send_message("The attachment is not an image or a GIF.", ephemeral=True)
-    else:
-        if message.content:
-            urls = re.findall(r'(https?://\S+)', message.content)
-            if urls:
-                for url in urls:
-                    if re.search(image_extensions_pattern, url, re.IGNORECASE):
-                        modal = GifType(url)
-                        await inter.response.send_modal(modal=modal)
-                        break
-                else:
-                    await inter.response.send_message("No valid image or GIF link found in the message content.", ephemeral=True)
+            if image.content_type == 'image/webp':
+                k = await image.read()
+                webp_image = Image.open(io.BytesIO(k))
+                png_image = webp_image.convert('RGBA')
+                png_buffer = io.BytesIO()
+                png_image.save(png_buffer, format='PNG')
+                png_buffer.seek(0)
+                msg = await inter.author.send("The image is being resent here to obtain it's url, please check the channel where you used your command for your GIF!", file=disnake.File(png_buffer, filename='converted_image.png'))
+                imgurl = msg.attachments[0].url
+                params = {'image_url': imgurl}
+                headers = {'Authorization': f'Bearer {os.getenv("JEYY_API_KEY")}'}
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(f'https://api.jeyy.xyz/v2/image/{type}', params=params, headers=headers) as response:
+                        buffer = io.BytesIO(await response.read())
+                        await inter.response.defer(ephemeral=True)
+                        await asyncio.sleep(6)
+                        await inter.edit_original_response(file=disnake.File(buffer, 'output.gif'))
+            elif image.content_type.startswith('image'):
+                params = {'image_url': image.url}
+                headers = {'Authorization': f'Bearer {os.getenv("JEYY_API_KEY")}'}
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(f'https://api.jeyy.xyz/v2/image/{type}', params=params, headers=headers) as response:
+                        buffer = io.BytesIO(await response.read())
+                        await inter.response.defer(ephemeral=True)
+                        await asyncio.sleep(6)
+                        await inter.edit_original_response(file=disnake.File(buffer, 'output.gif'))
             else:
-                await inter.response.send_message("No URLs found in the message content.", ephemeral=True)
-        else:
-            await inter.response.send_message("No attachment or message content found.", ephemeral=True)
+                await inter.response.send_message("The attachment is not an image.", ephemeral=True)
 
 @tasks.loop(count=1)
 async def patch_app_commands():
